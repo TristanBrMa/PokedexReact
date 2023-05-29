43122b1605f27b5a7e1c999443405b1d1d504b46
Maya Larribité <tristanbroussemalissard@gmail.com>
2023-05-29 17:02:19 +0200

    exo3react

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,25 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 const PokemonCard = () => {
+  const pokemon = pokemonList[0];
   return (
     <div>
       <figure>
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-          alt="bulbasaur"
-        />
-        <figcaption>bulbasaur</figcaption>
+        {pokemon.imgSrc ? (
+          <img src={pokemon.imgSrc} alt={pokemon.name} />
+        ) : (
+          <p>???</p>
+        )}
+        <figcaption>{pokemon.name}</figcaption>
       </figure>
     </div>
   );
