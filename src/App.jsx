27202613d1872f6589a41c15f8fd7367c7dc53b4
Maya Larribité <tristanbroussemalissard@gmail.com>
2023-05-31 21:17:10 +0200

    mise en place des states

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import PokemonCard from "./components/PokemonCard";
+import { useState } from "react";
 const pokemonList = [
   {
     name: "bulbasaur",
@@ -6,15 +7,42 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
-
 function App() {
-  const pokemon = pokemonList[0];
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  const handleClickMinus = () => {
+    setCount(count - 1);
+  };
+  // const isVisble = count !== 0;
+  // const isVisble2 = count !== pokemonList.length - 1;
+  const pokemon = pokemonList[count];
   return (
     <div>
       <PokemonCard name={pokemon.name} imgSrc={pokemon.imgSrc} />
+      {count > 0 && <button onClick={handleClickMinus}>précédent</button>}
+      {count < pokemonList.length - 1 && (
+        <button onClick={handleClick}>suivant</button>
+      )}
     </div>
   );
 }
